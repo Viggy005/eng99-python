1a88db8b008286522053db860fdfbc62ca239c09
vigneshraj venkatesh <vigneshrajvenkatesh@vigneshrajs-MacBook-Pro.local>
2021-11-19 16:45:55 +0530

    boolean

---
+++ boolean_intro.py
@@ -0,0 +1,29 @@
+# a = True
+# b = False
+#
+# print( 2+2 == 4)#eqivalence
+# print(2+2 != 4)# not eqivalence
+# print(5>6)
+# print(5<6)
+# print(type(6>6))
+# #print(a and b)
+# #print(a or b)
+# #print(not a)
+# hi = "hello"
+# print(hi.isalpha())
+# hi = "hello!"
+# print(hi.islower())
+# hi = "HELLO !"
+# print(hi.isupper())
+# hi = "hello!"
+# print(hi.endswith("!"))
+# hi = "hello!"
+# print(hi.startswith("hel"))
+
+#
+n = None
+print(bool(n))
+print(n is True)
+print(type(n))
+
+
